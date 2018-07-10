451f91a1cd5b4717e5ba28ceb7fd28b49b0e93ad
yedamao <logindaveye@gmail.com>
2018-07-10 11:31:40 +0800

    modify mockserver

--- cmd/mockserver/main.go
+++ cmd/mockserver/main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 
 	"github.com/yedamao/go_smgp/smgp/smgptest"
 )
@@ -23,9 +27,22 @@ func main() {
 		os.Exit(-1)
 	}
 
-	HandleSignals(server.Stop)
+	handleSignals(server.Stop)
 
 	server.Run()
 
 	fmt.Println("Done")
+}
+
+func handleSignals(stopFunction func()) {
+	var callback sync.Once
+
+	// On ^C or SIGTERM, gracefully stop the sniffer
+	sigc := make(chan os.Signal, 1)
+	signal.Notify(sigc, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-sigc
+		log.Println("service", "Received sigterm/sigint, stopping")
+		callback.Do(stopFunction)
+	}()
 }
